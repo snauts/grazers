fef227f5f09e720a1806b59cfe47395dbc73fabc
Artūrs Grebstelis <arturs@mikrotik.com>
2024-06-12 09:50:46 +0300

    add zx essentials

---
+++ Makefile
@@ -0,0 +1,25 @@
+CFLAGS += -mz80 --nostdinc --nostdlib --no-std-crt0
+CFLAGS += --code-loc $(CODE) --data-loc $(DATA)
+
+ENTRY = grep _reset grazers.map | cut -d " " -f 6
+
+all:
+	@echo "make zxs" - build .tap for ZX Spectrum
+	@echo "make fuse" - build and run fuse
+
+prg:
+	@sdcc $(CFLAGS) $(TYPE) main.c -o grazers.ihx
+	hex2bin grazers.ihx > /dev/null
+
+tap:
+	bin2tap -b -r $(shell printf "%d" 0x$$($(ENTRY))) grazers.bin
+
+zxs:
+	CODE=0x8000 DATA=0xf000	TYPE=-DZXS make prg
+	@make tap
+
+fuse: zxs
+	fuse --no-confirm-actions -g 2x grazers.tap
+
+clean:
+	rm -f grazers*
---
+++ main.c
@@ -0,0 +1,76 @@
+typedef signed char int8;
+typedef unsigned char byte;
+typedef unsigned short word;
+
+#define ADDR(obj)	((word) (obj))
+#define BYTE(addr)	(* (volatile byte *) (addr))
+#define WORD(addr)	(* (volatile word *) (addr))
+#define SIZE(array)	(sizeof(array) / sizeof(*(array)))
+
+#ifdef ZXS
+#define is_vsync()	vblank
+#define SETUP_STACK()	__asm__("ld sp, #0xFDFC")
+#define IRQ_BASE	0xfe00
+#endif
+
+static volatile byte vblank;
+
+static void interrupt(void) __naked {
+#ifdef ZXS
+    __asm__("di");
+    __asm__("push af");
+    __asm__("ld a, #1");
+    __asm__("ld (_vblank), a");
+    __asm__("pop af");
+    __asm__("ei");
+#endif
+    __asm__("reti");
+}
+
+static void __sdcc_call_hl(void) __naked {
+    __asm__("jp (hl)");
+}
+
+static void setup_irq(byte base) {
+    __asm__("di");
+    __asm__("ld i, a"); base;
+    __asm__("im 2");
+    __asm__("ei");
+}
+
+static void out_fe(byte data) __naked {
+    __asm__("out (#0xfe), a"); data;
+    __asm__("ret");
+}
+
+static byte in_fe(byte a) __naked {
+    __asm__("in a, (#0xfe)"); a;
+    __asm__("ret");
+}
+
+static void memset(byte *ptr, byte data, word len) {
+    while (len-- > 0) { *ptr++ = data; }
+}
+
+static void setup_system(void) {
+    byte top = (byte) ((IRQ_BASE >> 8) - 1);
+    word jmp_addr = (top << 8) | top;
+    BYTE(jmp_addr + 0) = 0xc3;
+    WORD(jmp_addr + 1) = ADDR(&interrupt);
+    memset((byte *) IRQ_BASE, top, 0x101);
+    setup_irq(IRQ_BASE >> 8);
+}
+
+static void clear_screen(void) {
+#ifdef ZXS
+    memset((byte *) 0x5800, 0x00, 0x300);
+    memset((byte *) 0x4000, 0x00, 0x1800);
+    out_fe(0);
+#endif
+}
+
+void reset(void) {
+    SETUP_STACK();
+    setup_system();
+    clear_screen();
+}
