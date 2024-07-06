a03f2e22beeda65029cde2e0e31e70ee0a051e1a
Arturs Grebstelis <arturs.grebstelis@gmail.com>
2024-07-06 19:04:59 +0300

    fix migration sprites on msx

--- main.c
+++ main.c
@@ -1198,7 +1198,7 @@ static void put_wave(word n, byte tile, byte color) {
 
 #ifdef MSX
     word addr = n + 0x5800;
-    if (color) tile += 15;
+    if (color) tile += 16;
     vram_write(addr, 40 + tile);
 #endif
 }
